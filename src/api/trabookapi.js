14aaa939d90709e3b95efcde0c4c35d363e8c88e
Andres Montes <hypnoz@live.com>
2023-11-16 21:26:33 -0500

    Update trabookapi.js

--- src/api/trabookapi.js
+++ src/api/trabookapi.js
@@ -1,6 +1,7 @@
 const getCards = async() => {
     try{
-        const response = await fetch('http://127.0.0.1:8000/api/cards/');
+        /* const response = await fetch('http://127.0.0.1:8000/api/cards/'); */
+        const response = await fetch('https://trabook.onrender.com/api/cards/');
         if(!response.ok){
             throw new Error("Error al conectar con api");
         }
